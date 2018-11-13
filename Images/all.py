6d309c1d99cb1088155b62c1ccb23cc082f233ef
Usamamalik007 <Usamamalik21@gmail.com>
2018-11-14 02:31:05 +0500

    Update Images/all.py

--- Images/all.py
+++ Images/all.py
@@ -14,21 +14,28 @@ thisdict ={
   "0":"کمرہ",
 }
 
-import cv2
-import numpy as np
+x=0
+y=0
+while x<10:
+    y=0
+    while y<10:
+        img1 = cv2.imread(str(x)+'.jpg')
+        img2 = cv2.imread(str(y)+'.jpg')
+        vis = np.concatenate((img1, img2), axis=1)
+        cv2.imwrite(str(x)+str(y)+ '.png', vis)
+        with open(str(x)+str(y)+'.gt.txt', 'w',encoding="utf-8") as f:
+            a = thisdict.get(str(x))
+            b = thisdict.get(str(y))
+            f.write(str(b) + " "+str(a) )
+        print("y= "+str(y))
+        y=y+1
+    x=x+1
+    print("x= "+str(x))
+    
+        
 
-thisdict ={
-  "1":"میں",
-  "2":"تم" ,
-  "3":"کیا" ,
-  "4":"سب",
-  "5":"صبر",
-  "6":"اگر" ,
-  "7":"کرسی",
-  "8":"بوتل",
-  "9":"موبائل",
-  "0":"کمرہ",
-}
+
+        
 
 
         
